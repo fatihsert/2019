0f9b82e443f40f9fb1c419df1501942b895bba90
fatihsert <fathsrt@gmail.com>
2019-10-04 02:00:20 +0300

    solution created

---
+++ OOPPrinciples/Encapsulation/HtmlHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPPrinciples.Encapsulation
+{
+    public class Rectangle
+    {
+        private int width;
+        private int height;
+
+        public bool SetHeigth(int height)
+        {
+            if (height < 0)
+            {
+                return false;
+            }
+
+            this.height = height;
+
+            return true;
+        }
+
+        public bool SetWidth(int width)
+        {
+            if (width<0)
+            {
+                return false;
+            }
+
+            this.width = width;
+
+            return true;
+        }
+
+    }
+}
---
+++ OOPPrinciples/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OOPPrinciples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encapsulation.Rectangle rectangle = new Encapsulation.Rectangle();
+            rectangle.SetHeigth(10);
+            rectangle.SetWidth(20);
+        }
+    }
+}
